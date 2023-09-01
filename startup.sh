70ac149d4c285a0d21d7acf369ba5b22fedff5dc
Ajibola <davidajibolatimilehin@gmail.com>
2023-09-01 12:46:46 +0100

    changed port to 8000

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install -y libgl1-mesa-glx
+apt-get install -y libgl1-mesa-glx libglib2.0-dev
 
 
-gunicorn --bind=0.0.0.0:80 --timeout 600 app:app
+gunicorn --bind=0.0.0.0:8000 --timeout 600 app:app
